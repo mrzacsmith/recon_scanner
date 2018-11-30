7ec061556b81deabb50aecc9c5c5c2776d251303
Zac Smith <zrs3141592@gmail.com>
2018-11-30 01:41:19 +0000

    function to use python to open and run nmap scan

---
+++ nmap.py
@@ -0,0 +1,13 @@
+import os
+
+def get_nmap(options, ip):
+    command = "nmap " + options + " " + ip
+    process = os.popen(command)
+    
+    results = str(process.read())
+    
+    return results
+
+print(get_nmap('-F', '74.125.124.139'))
+
+
