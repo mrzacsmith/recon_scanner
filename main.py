4b0705b8b1f64408bde86bde6a5b949adc2f074c
Zac Smith <zrs3141592@gmail.com>
2018-12-04 00:41:41 +0000

    start main from scratch

--- main.py
+++ main.py
@@ -4,27 +4,3 @@ from ip_address import *
 from nmap import *
 from robots_txt import *
 from whois import *
-
-ROOT_DIR = 'targets'
-create_dir(ROOT_DIR)
-
-def gather_info(name, url):
-    robots_txt = get_robots_txt(url)
-    domain_name = get_domin_name(url)
-    ip_address = get_ip_address(url)
-    nmap = get_nmap('-F', ip_address)
-    whois = get_whois(domain_name)
-    
-    create_report(name, url, domain_name, nmap, robots_txt, whois)
-    
-def create_report(name, url, domain_name, nmap, robots_txt, whois):
-    project_dir = ROOT_DIR + '/' + name
-    create_dir(project_dir)
-
-    write_file(project_dir + "/full_url.txt", url)
-    write_file(project_dir + "/domain_name.txt", domain_name)
-    write_file(project_dir + "/robots_txt.txt", robots_txt)
-    write_file(project_dir + "/nmap.txt", nmap)
-    write_file(project_dir + "/whois.txt", whois)
-    
-gather_info('google.com', 'https://wwww.google.com')
